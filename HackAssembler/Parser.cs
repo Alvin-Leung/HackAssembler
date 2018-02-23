59ce49c6740d5990d2b4b2b8a731978c055437cd
Alvin Leung <alvin.leung92@gmail.com>
2018-02-22 22:54:09 -0800

    Create Parser class logic. Instances of the Parser class can take an assembly command string and output an Instruction object.

--- HackAssembler/Parser.cs
+++ HackAssembler/Parser.cs
@@ -1,12 +1,93 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace HackAssembler
 {
-    class Parser
+    public class Parser
     {
+        public Instruction GetInstructionFromAssemblyCommand(string assemblyCommand)
+        {
+            try
+            {
+                Instruction instruction = TryGetInstructionFromAssemblyCommand(assemblyCommand);
+
+                return instruction;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private Instruction TryGetInstructionFromAssemblyCommand(string assemblyCommand)
+        {
+            Instruction instruction;
+
+            if (SyntaxValidator.IsAddressInstruction(assemblyCommand))
+            {
+                instruction = GetAddressInstruction(assemblyCommand);
+            }
+            else if (SyntaxValidator.IsComputationInstruction(assemblyCommand))
+            {
+                instruction = GetComputationInstruction(assemblyCommand);
+            }
+            else
+            {
+                throw new Exception("Parser.GetInstructionFromAssemblyCommand - Instruction is not valid");
+            }
+
+            return instruction;
+        }
+
+        private Instruction GetComputationInstruction(string assemblyCommand)
+        {
+            Instruction cInstruction;
+
+            string destination = String.Empty;
+
+            string computation;
+
+            string jump = String.Empty;
+
+            string[] destAndCompCommand;
+
+            string[] compAndJumpCommand;
+
+            assemblyCommand = assemblyCommand.Replace(" ", "");
+
+            destAndCompCommand = assemblyCommand.Split('=');
+
+            if (destAndCompCommand.Length == 2)
+            {
+                destination = destAndCompCommand[0];
+
+                compAndJumpCommand = destAndCompCommand[1].Split(';');
+            }
+            else
+            {
+                compAndJumpCommand = destAndCompCommand[0].Split(';');
+            }
+
+            computation = compAndJumpCommand[0];
+
+            if (compAndJumpCommand.Length == 2)
+            {
+                jump = compAndJumpCommand[1];
+            }
+
+            cInstruction = new ComputationInstruction(destination, computation, jump);
+
+            return cInstruction;
+        }
+
+        private Instruction GetAddressInstruction(string assemblyCommand)
+        {
+            string address = assemblyCommand.Remove(0, 1);
+
+            Instruction addressInstruction = new AddressInstruction(address);
+
+            return addressInstruction;
+        }
     }
 }
