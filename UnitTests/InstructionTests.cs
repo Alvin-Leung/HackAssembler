b577dbee3683df0054025263b77f4b18b17582ac
Alvin Leung <alvin.leung92@gmail.com>
2018-02-24 19:34:24 -0800

    Create unit tests for Instruction interface implementations

---
+++ UnitTests/InstructionTests.cs
@@ -0,0 +1,33 @@
+﻿using HackAssembler;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class InstructionTests
+    {
+        [TestMethod]
+        public void AInstruction_InstantiateWithValidAddress_BinaryInstructionIsCorrect()
+        {
+            AInstruction aInstruction = new AInstruction("4578");
+
+            Assert.AreEqual("0001000111100010", aInstruction.GetInstructionAsBinary());
+        }
+
+        [TestMethod]
+        public void CInstruction_InstantiateWithValidInputs_BinaryInstructionIsCorrect()
+        {
+            CInstruction cInstruction = new CInstruction("M", "M+1", "JMP");
+
+            Assert.AreEqual("1111110111001111", cInstruction.GetInstructionAsBinary());
+        }
+
+        [TestMethod]
+        public void CInstruction_InstantiateWithValidInputs2_BinaryInstructionIsCorrect()
+        {
+            CInstruction cInstruction = new CInstruction("AM", "D&M", "JLE");
+
+            Assert.AreEqual("1111000000101110", cInstruction.GetInstructionAsBinary());
+        }
+    }
+}
