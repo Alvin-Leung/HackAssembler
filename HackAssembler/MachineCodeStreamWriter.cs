d9fb67eb6901698bdaf412a4f59bf90685aa1098
Alvin Leung <alvin.leung92@gmail.com>
2018-02-25 22:37:29 -0800

    Add MachineCodeStreamWriter class that writes machine code array to file

---
+++ HackAssembler/MachineCodeStreamWriter.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace HackAssembler
+{
+    public class MachineCodeStreamWriter
+    {
+        private string filepath;
+
+        public MachineCodeStreamWriter(string filepath)
+        {
+            this.filepath = filepath;
+        }
+
+        public void WriteToFile(string[] machineCodeInstructions)
+        {
+            using (StreamWriter writer = new StreamWriter(filepath))
+            {
+                foreach (string instruction in machineCodeInstructions)
+                {
+                    writer.WriteLine(instruction);
+                }
+            }
+        }
+    }
+}
