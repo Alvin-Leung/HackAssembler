680028144f61737f7bd7be047f6ff3fc6fee0711
Alvin Leung <alvin.leung92@gmail.com>
2018-03-03 18:29:39 -0800

    Add SymbolHandler class

---
+++ HackAssembler/SymbolHandler.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace HackAssembler
+{
+    public class SymbolHandler
+    {
+        private SymbolTable symbolTable;
+
+        public SymbolHandler(string[] assemblyInstructions)
+        {
+            Dictionary<string, int> labelDictionary = new Dictionary<string, int>();
+
+            string label;
+
+            int instructionLineNumber = 0;
+
+            foreach (string instruction in assemblyInstructions)
+            {
+                if (SyntaxValidator.IsLabel(instruction))
+                {
+                    label = instruction.Trim('(', ')');
+
+                    labelDictionary.Add(label, instructionLineNumber);
+                }
+                else
+                {
+                    instructionLineNumber++;
+                }
+            }
+
+            symbolTable = new SymbolTable(labelDictionary);
+        }
+
+        public string[] SubstituteSymbolsWithValues(string[] assemblyInstructions)
+        {
+            List<string> assemblyInstructionsWithoutSymbols = new List<string>();
+
+            foreach (string instruction in assemblyInstructions)
+            {
+                if (!SyntaxValidator.IsLabel(instruction))
+                {
+                    if (SyntaxValidator.IsAddressInstructionWithSymbol(instruction))
+                    {
+                        string addressSymbol = instruction.Remove(0, 1);
+
+                        if (!symbolTable.Contains(addressSymbol))
+                        {
+                            symbolTable.AddVariable(addressSymbol);
+                        }
+
+                        assemblyInstructionsWithoutSymbols.Add("@" + symbolTable.GetValue(addressSymbol));
+                    }
+                    else
+                    {
+                        assemblyInstructionsWithoutSymbols.Add(instruction);
+                    }
+                }
+            }
+
+            return assemblyInstructionsWithoutSymbols.ToArray();
+        }
+    }
+}
