b81619f52f27c1cc72f43d9017dc70daa74e9be2
Alvin Leung <alvin.leung92@gmail.com>
2018-03-03 19:11:22 -0800

    Add Assembler class and implement its methods for converting .asm files to .hack files

---
+++ HackAssembler/Assembler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace HackAssembler
+{
+    static public class Assembler
+    {
+        static public void TryConvertAsmToHack(string filepath)
+        {
+            try
+            {
+                FileInfo fileInfo = new FileInfo(filepath);
+
+                ConvertAsmToHack(filepath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static public void ConvertAsmToHack(string filepath)
+        {
+            AssemblyFileParser assemblyFileParser = new AssemblyFileParser(filepath);
+
+            string[] assemblyInstructions = assemblyFileParser.GetArrayOfAssemblyInstructions();
+
+            Console.WriteLine("Assembly file parsed successfully...");
+
+            string[] machineCodeInstructions = AssemblyLanguageParser.GetMachineCodeArray(assemblyInstructions);
+
+            Console.WriteLine("Assembly language converted to machine code successfully...");
+
+            FileInfo fileInfo = new FileInfo(filepath);
+
+            string machineCodeFilePath = Path.Combine(fileInfo.DirectoryName, fileInfo.Name.Replace(fileInfo.Extension, ".hack"));
+
+            MachineCodeStreamWriter machineCodeStreamWriter = new MachineCodeStreamWriter(machineCodeFilePath);
+
+            machineCodeStreamWriter.WriteToFile(machineCodeInstructions);
+
+            Console.WriteLine("Machine code file generation complete");
+        }
+    }
+}
